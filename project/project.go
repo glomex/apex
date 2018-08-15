edce2f15338acad97df95b616aa65c27df5a23a2
Serhii Khylyk <serhii.khylyk@glomex.com>
2018-08-15 12:16:51 +0300

    Add check

--- project/project.go
+++ project/project.go
@@ -180,6 +180,7 @@ func (p *Project) LoadFunctions(patterns ...string) error {
 // DeployAndClean deploys functions and then cleans up their build artifacts.
 func (p *Project) DeployAndClean() error {
 	p.Log.Info(p.Role)
+	p.Log.Info(readInfraRole())
 	if err := p.Deploy(); err != nil {
 		return err
 	}
