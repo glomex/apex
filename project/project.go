60dc5f20fdec34e6a720e322c63f092e23b5ed5a
Serhii Khylyk <serhii.khylyk@glomex.com>
2018-08-15 15:54:23 +0300

    Add

--- project/project.go
+++ project/project.go
@@ -200,24 +200,24 @@ func (p *Project) checkRole() bool {
 		RoleName: aws.String(funcName),
 	}
 	svc := iam.New(p.Session)
-	result, _ := svc.GetRole(input)
-	//if err != nil {
-	//	if aerr, ok := err.(awserr.Error); ok {
-	//		switch aerr.Code() {
-	//		case iam.ErrCodeNoSuchEntityException:
-	//			fmt.Println(iam.ErrCodeNoSuchEntityException, aerr.Error())
-	//		case iam.ErrCodeServiceFailureException:
-	//			fmt.Println(iam.ErrCodeServiceFailureException, aerr.Error())
-	//		default:
-	//			fmt.Println(aerr.Error())
-	//		}
-	//	} else {
-	//		// Print the error, cast err to awserr.Error to get the Code and
-	//		// Message from an error.
-	//		fmt.Println(err.Error())
-	//	}
-	//	return false
-	//}
+	result, err := svc.GetRole(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case iam.ErrCodeNoSuchEntityException:
+				fmt.Println(iam.ErrCodeNoSuchEntityException, aerr.Error())
+			case iam.ErrCodeServiceFailureException:
+				fmt.Println(iam.ErrCodeServiceFailureException, aerr.Error())
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+		return false
+	}
 
 	fmt.Println(input)
 	fmt.Println(result)
